437cd80a6c7fc0843be3c03517057da2e384bd74
jt15 <jthota01@gmail.com>
2020-01-23 08:53:50 -0600

    fixed file paths

--- src/main/java/edu/mcw/scge/configuration/SecurityConfiguration.java
+++ src/main/java/edu/mcw/scge/configuration/SecurityConfiguration.java
@@ -51,8 +51,8 @@ import java.util.stream.Collectors;
  */
 
 @EnableWebSecurity
-@PropertySource("classpath:application.properties")
-
+@PropertySource("/home/rgdpub/data/application.properties")
+//@PropertySource("C:/Apps/application.properties")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
    private static List<String> clients = Arrays.asList("google");
 
