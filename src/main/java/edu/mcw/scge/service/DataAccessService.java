34ef002f8eb7d7faeea6901266f8c6ff0eca6e16
Thota <jthota@mcw.edu>
2021-04-08 11:41:36 -0500

    code cleanup

--- src/main/java/edu/mcw/scge/service/DataAccessService.java
+++ src/main/java/edu/mcw/scge/service/DataAccessService.java
@@ -47,15 +47,6 @@ public class DataAccessService extends AbstractDAO {
             p.setId(key);
             pdao.insert(p);
 
-     //    return pdao.getPerson(p).get(0).getId();
-
-    }
-    public boolean existsPerson(Person p) throws Exception{
-        List<Person> persons=pdao.getPersonByEmail(p.getEmail());
-        if(persons.size()>0){
-            return true;
-        }
-        return false;
     }
    public boolean existsInSCGE(GoogleIdToken.Payload payload) throws Exception {
        List<Person> p=pdao.getPersonByGoogleId(payload.getSubject());
@@ -74,22 +65,7 @@ public class DataAccessService extends AbstractDAO {
 
         return false;
     }
- /*   public boolean existsInSCGE(GoogleIdToken.Payload payload) throws Exception {
-        List<Person> p=pdao.getPersonByGoogleId(payload.getSubject());
-        if(p==null || p.size()==0){
-            URI uri = new URI("https://groups.io/api/v1/login?", "email=jthota@mcw.edu&password=testingfb02&api_key=nonce", null);
-            String str = uri.toASCIIString();
-           String jsonObject=restGet(str,null);
-            System.out.println("jsonObject:"+jsonObject);
 
-        }else{
-            if(p.size()>0)
-                return true;
-        }
-
-
-        return false;
-    }*/
     public static String restGet(String url, String contentType) throws Exception {
         try {
             DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -126,59 +102,11 @@ public class DataAccessService extends AbstractDAO {
             if(pList!=null && pList.size()==1){
                 pdao.updateGoogleId(googleSub,pList.get(0).getId() );
                 return  pList.get(0).getId();
-            }/*else{
-                pList= pdao.getPersonByLastName(familyName);
-                if(pList!=null && pList.size()==1){
-                    pdao.updateGoogleId(googleSub,pList.get(0).getId() );
-                    return  pList.get(0).getId();
-                }
-            }*/
+            }
 
         return 0;
     }
 
-/*    public boolean isGeneralAdmin(GoogleIdToken.Payload payload) throws Exception {
-        String email=payload.getEmail();
-        List<Person> records=pdao.getPersonByEmail(email);
-        if(records.size()>0){
-            Person p=records.get(0);
-          List<String> groups= pdao.getPersonGroups(p);
-       //     if(groups.contains("general_admin"))
-            if(groups.contains("general")) {
-              String access=  pdao.getGroupAccessLevel(p, "general").get(0);
-                if(access.equalsIgnoreCase("admin"))
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean isGroupAdmin(GoogleIdToken.Payload payload) throws Exception {
-        String email=payload.getEmail();
-        List<Person> records=pdao.getPersonByEmail(email);
-        if(records.size()>0){
-            Person p=records.get(0);
-            List<String> groups= pdao.getPersonGroups(p);
-            //     if(groups.contains("general_admin"))
-            if(groups.contains("general")) {
-                String access=  pdao.getGroupAccessLevel(p, "general").get(0);
-                if(access.equalsIgnoreCase("group_admin"))
-                    return true;
-            }
-        }
-        return false;
-    }
-*/
-    public String getUserStatus(GoogleIdToken.Payload payload) throws Exception {
-          String status=new String();
-       //   status=pdao.getPersonStatus(payload.getSubject());
-        status=pdao.getPersonStatus(payload.getSubject());
-
-        return status;
-
-    }
-    public List<Person> getAllUnauthorizedUsers() throws Exception {
-      return   pdao.getAllUnauthorizedUsers();
-     }
     public List<Person> getAllMembers() throws Exception {
         return   pdao.getAllActiveMembers();
     }
@@ -192,121 +120,9 @@ public class DataAccessService extends AbstractDAO {
         }
         return  0 ;
     }
-    public void updateUserStatus(HttpServletRequest req) throws Exception {
-           Person person= new Person.Builder()
-                .name(req.getParameter("name"))
-                .email(req.getParameter("email"))
-                .status(req.getParameter("status"))
-                .modifiedBy((String)req.getSession().getAttribute("userEmail"))
-                .modifiedById((String)req.getSession().getAttribute("userId"))
-                .build();
-        pdao.updateStatus(person);
-        List<Person> p=pdao.getPerson(person);
-      //  System.out.println(person.getName()+"\t"+ person.getEmail()+"\tPERSON SIZE: "+ p.size());
-        if(p!=null && p.size()>0){
-            Person person1=p.get(0);
-            int personId=  person1.getId();
-       //     int groupKey=gdao.getGroupKey("general");
-        //    int accessId=adao.getAccessId("member");
-        //    pdao.addMemberToGroup(personId, groupKey, accessId);
-        }
 
-    }
-    public Map<String, List<String>> getGroupsByMemberName(String userName) {
-        Map<String, List<String>> groupRoleMap=new HashMap<>();
-        try {
-            groupRoleMap=   gdao.getGroupsNRoles(userName);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-      //  System.out.println("groupROLEMAP"+groupRoleMap.size());
-        return groupRoleMap;
-    }
-    public Map<String, List<String>> getGroupsNRolesByMemberId(int id) {
-        Map<String, List<String>> groupRoleMap=new HashMap<>();
-        try {
-            groupRoleMap=   gdao.getGroupsNRolesByMemberId(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    //    System.out.println("groupROLEMAP"+groupRoleMap.size());
-        return groupRoleMap;
-    }
-  /*  public Map<String,Map<String, List<String>>> getGroupsByPersonId(int id) {
-       Map<String, Map<String, List<String>>> groupSubgroupRoleMap=new HashMap<>();
-        try {
-            List<PersonInfo> associations=  gdao.getGroupsNRolesByPersonId(id);
-            for(PersonInfo i:associations){
-                Map<String, List<String>> map=groupSubgroupRoleMap.get(i.getGroupName());
-                List<String> roles=new ArrayList<>();
-                if(map!=null && map.size()>0){
 
-                    if(map.get(i.getSubGroupName())!=null){
-                        roles.addAll(map.get(i.getSubGroupName()));
-                     }
 
-                }else map=new HashMap<>();
-                roles.add(i.getRole());
-                map.put(i.getSubGroupName(), roles);
-                groupSubgroupRoleMap.put(i.getGroupName(), map);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //    System.out.println("groupROLEMAP"+groupRoleMap.size());
-        return groupSubgroupRoleMap;
-    }*/
-    public List<Person> getGroupMembers(String groupName) throws Exception {
-        if(groupName.contains("Cell")){
-            groupName="Cell & Tissue Platform";
-        }
-      List<Person> members= gdao.getGroupMembers(groupName);
-
-        Set<Integer> personIds=new HashSet<>();
-        List<Person> sortedMembersList=new ArrayList<>();
-        for(Person p:members){
-
-            if(!personIds.contains(p.getId())){
-                personIds.add(p.getId());
-                p.setRoles(pdao.getRolesByPersonId(p.getId(), groupName));
-                sortedMembersList.add(p);
-            }
-        }
-      //  System.out.println("MEMBERS SIZE: "+ members.size());
-        return sortedMembersList;
-    }
-  /*  public List<String> getSubGroupsByGroupName(String groupName) {
-        if(groupName.contains("Cell")){
-            groupName="Cell & Tissue Platform";
-        }
-
-        List<String> subgroups=new ArrayList<>();
-        try {
-            subgroups=   gdao.getSubGroupsByGroupName(groupName);
-            } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return subgroups;
-    }*/
-  /*  public  Map<String, List<String>> getGroupsNRolesMapByGroupName(String groupName) {
-        if(groupName.contains("Cell")){
-            groupName="Cell & Tissue Platform";
-        }
-        Map<String, List<String>> map= new HashMap<>();
-        List<String> subgroups=new ArrayList<>();
-        try {
-            subgroups=   gdao.getSubGroupsByGroupName(groupName);
-            for(String sg:subgroups){
-                List<String> ssgroups=  gdao.getSubGroupsByGroupName(sg);
-                map.put(sg, ssgroups);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return map;
-    }*/
    public  Map<SCGEGroup, List<SCGEGroup>> getGroupsMapByGroupName(String groupName) throws Exception {
 
         Map<SCGEGroup, List<SCGEGroup>> map= new HashMap<>();
@@ -324,82 +140,8 @@ public class DataAccessService extends AbstractDAO {
 
         return map;
     }
- /*   public  Map<SCGEGroup, List<SCGEGroup>> getGroupMapByGroupId(int groupId) {
 
-        Map<SCGEGroup, List<SCGEGroup>> map= new HashMap<>();
-        List<SCGEGroup> subgroups=new ArrayList<>();
-        try {
-            subgroups=   gdao.getSubGroupsByGroupId(groupId);
-            for(SCGEGroup sg:subgroups){
-                List<SCGEGroup> ssgroups=  gdao.getSubGroupsByGroupId(sg.getGroupId());
-                for(SCGEGroup g:ssgroups){
-                    List<Person> members=new ArrayList<>();
-                    Set<Integer> memberIds=new HashSet<>();
-                    for(Person p:gdao.getGroupMembersByGroupId(g.getGroupId())) {
-                      if(!memberIds.contains(p.getId())){
-                          memberIds.add(p.getId());
-                          members.add(p);
-                      }
-                    }
-                    g.setMembers(members);
-                }
-                map.put(sg, ssgroups);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        return map;
-    }*/
-  /*  public  Map<SCGEGroup, List<SCGEGroup>> getGroupMapByGroupName(String groupName) throws Exception {
-
-        Map<SCGEGroup, List<SCGEGroup>> map= new HashMap<>();
-        List<SCGEGroup> subgroups=new ArrayList<>();
-        int groupId=gdao.getGroupId(groupName);
-        try {
-            subgroups=   gdao.getSubGroupsByGroupId(groupId);
-            for(SCGEGroup sg:subgroups){
-                List<SCGEGroup> ssgroups=  gdao.getSubGroupsByGroupId(sg.getGroupId());
-                for(SCGEGroup g:ssgroups){
-                    List<Person> members=new ArrayList<>();
-                    Set<Integer> memberIds=new HashSet<>();
-                    for(Person p:gdao.getGroupMembersByGroupId(g.getGroupId())) {
-                        if(!memberIds.contains(p.getId())){
-                            memberIds.add(p.getId());
-                            members.add(p);
-                        }
-                    }
-                    g.setMembers(members);
-                }
-                map.put(sg, ssgroups);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return map;
-    }*/
-    public Map<Integer, List<Person>> getAllGroupsMembersMap( Map<Integer, List<SCGEGroup>> consortiumGroup) throws Exception {
-        Map<Integer, List<Person>> map=new HashMap<>();
-        for(Map.Entry e: consortiumGroup.entrySet()){
-            List<SCGEGroup> groups= (List<SCGEGroup>) e.getValue();
-            for(SCGEGroup g:groups){
-                int groupId= g.getGroupId();
-                List<Person> sortedMembers=new ArrayList<>();
-                List<Integer> memberIds=new ArrayList<>();
-                List<Person> members=gdao.getGroupMembersByGroupId(groupId);
-                for(Person p:members){
-                    if(!memberIds.contains(p.getId())){
-                        sortedMembers.add(p);
-                        memberIds.add(p.getId());
-                    }
-                }
-                map.put(groupId, sortedMembers);
-
-            }
-        }
-        return map;
-    }
     public Map<SCGEGroup, List<Person>> getGroupMembersMapExcludeDCCNIH( Map<SCGEGroup, List<SCGEGroup>> consortiumGroup) throws Exception {
         Map<SCGEGroup, List<Person>> map=new HashMap<>();
         List<Integer> DCCNIHGroups= gdao.getDCCNIHGroupIds();
@@ -479,15 +221,9 @@ public class DataAccessService extends AbstractDAO {
         for(Study s:studies){
             List<Integer> groups=new ArrayList<>();
            for(StudyAssociation a: sdao.getStudyAssociations(s.getStudyId())){
-              // SCGEGroup g= new SCGEGroup();
-             //  g=gdao.getGroupById(a.getGroupId());
-              // g.setMembers( gdao.getGroupMembersByGroupId(a.getGroupId()));
                groups.add(a.getGroupId());
             }
            for(StudyTierUpdate u: tierUpdateDao.getStudyTierUpdatesByStudyId(s.getStudyId())){
-            //   SCGEGroup g=new SCGEGroup();
-           //    g=gdao.getGroupById(u.getGroupId());
-             //  g.setMembers( gdao.getGroupMembersByGroupId(u.getGroupId()));
                groups.add(u.getAssociatedGroupId());
            }
            s.setAssociatedGroups(groups);
@@ -583,14 +319,7 @@ public class DataAccessService extends AbstractDAO {
         }
         insertTierUpdates(updates);
     }
-    public Map<String, Map<Integer, Integer>> sortStudies(List<Study> studies){
-        Map<String, Map<Integer, Integer>> sortedMap=new HashMap<>();
-        for(Study s:studies){
 
-        }
-
-        return null;
-    }
     public LinkedHashMap<String, List<Integer>> getPlotData() throws Exception {
         GrantDao grantDao=new GrantDao();
         Map<String, Map<Integer, Integer>> statsMap=new HashMap<>();
@@ -628,8 +357,6 @@ public class DataAccessService extends AbstractDAO {
         for(Map.Entry e:statsMap.entrySet()){
             String initative= (String) e.getKey();
             labels.add(getLabel(initative));
-
-            //   System.out.println("Initiative: "+ initative+"*********");
             Map<Integer, Integer> tierCounts= (Map<Integer, Integer>) e.getValue();
             List<Integer> submissions=new ArrayList<>();
             List<Integer> tier1=new ArrayList<>();
