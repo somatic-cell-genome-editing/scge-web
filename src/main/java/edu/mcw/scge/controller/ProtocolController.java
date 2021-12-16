82cad81c8a2c4dd8e37b2385407e8a118393ad35
harika91 <harika91@users.noreply.github.com>
2021-12-16 07:23:04 -0600

    Update ProtocolController.java

--- src/main/java/edu/mcw/scge/controller/ProtocolController.java
+++ src/main/java/edu/mcw/scge/controller/ProtocolController.java
@@ -5,8 +5,13 @@ import edu.mcw.scge.configuration.UserService;
 import edu.mcw.scge.dao.implementation.*;
 import edu.mcw.scge.datamodel.*;
 import edu.mcw.scge.service.db.DBService;
+import edu.mcw.scge.storage.FileSystemStorageService;
+import edu.mcw.scge.storage.StorageProperties;
 import edu.mcw.scge.web.UI;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -55,4 +60,82 @@ public class ProtocolController {
 
         return null;
     }
+    @RequestMapping(value = "/edit")
+    public String getProtocolForm(HttpServletRequest req, HttpServletResponse res,Protocol protocol) throws Exception{
+
+        UserService userService = new UserService();
+        Person p=userService.getCurrentUser(req.getSession());
+        edu.mcw.scge.configuration.Access access = new Access();
+
+        if(!access.isLoggedIn()) {
+            return "redirect:/";
+        }
+
+        if (!access.isInDCCorNIHGroup(p)) {
+            req.setAttribute("page", "/WEB-INF/jsp/error");
+            req.getRequestDispatcher("/WEB-INF/jsp/base.jsp").forward(req, res);
+            return null;
+
+        }
+
+        if(req.getParameter("id") != null) {
+           Protocol pro = protocolDao.getProtocolById(Long.parseLong(req.getParameter("id")));
+            req.setAttribute("protocol",pro);
+            req.setAttribute("action","Update Protocol");
+        }else {
+            req.setAttribute("protocol", new Protocol());
+            req.setAttribute("action", "Create Protocol");
+        }
+        req.setAttribute("page", "/WEB-INF/jsp/edit/editProtocol");
+        req.setAttribute("crumbtrail","<a href='/toolkit/loginSuccess?destination=base'>Home</a> / <a href='/toolkit/data/protocols/search'>Protocols</a>");
+        req.getRequestDispatcher("/WEB-INF/jsp/base.jsp").forward(req, res);
+
+        return null;
+    }
+    @RequestMapping("/create")
+    public String createModel(HttpServletRequest req,HttpServletResponse res,@ModelAttribute("protocol") Protocol protocol) throws Exception {
+
+        long protocolId = protocol.getId();
+        UserService userService = new UserService();
+        Person p=userService.getCurrentUser(req.getSession());
+        edu.mcw.scge.configuration.Access access = new Access();
+
+        if(!access.isLoggedIn()) {
+            return "redirect:/";
+        }
+
+        if (!access.isInDCCorNIHGroup(p)) {
+            req.setAttribute("page", "/WEB-INF/jsp/error");
+            req.getRequestDispatcher("/WEB-INF/jsp/base.jsp").forward(req, res);
+            return null;
+
+        }
+        String filename = StringUtils.cleanPath(protocol.getFile().getOriginalFilename());
+        protocol.setFilename(filename);
+        if(protocolId == 0) {
+            protocolId = protocolDao.getProtocolId(protocol);
+            if(protocolId == 0) {
+                protocolId = protocolDao.insertProtocol(protocol);
+                StorageProperties properties=new StorageProperties();
+                properties.setLocation("/data/scge_protocols");
+                FileSystemStorageService service=new FileSystemStorageService(properties);
+                service.store(protocol.getFile());
+
+                req.setAttribute("status"," <span style=\"color: blue\">Protocol added successfully</span>");
+            }else {
+                req.setAttribute("status"," <span style=\"color: red\">Protocol already exists</span>");
+            }
+        }else {
+            protocolDao.updateProtocol(protocol);
+            StorageProperties properties=new StorageProperties();
+            properties.setLocation("/data/scge_protocols");
+            FileSystemStorageService service=new FileSystemStorageService(properties);
+            service.store(protocol.getFile());
+            req.setAttribute("status"," <span style=\"color: blue\">Protocol updated successfully</span>");
+        }
+
+        req.getRequestDispatcher("/data/protocols/protocol?id="+protocolId).forward(req,res);
+        return null;
+    }
+
 }
