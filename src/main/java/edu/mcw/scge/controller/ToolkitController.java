6be7266d7a8db57775b55c185ab8374aa9c5290c
De Pons <jdepons@rgd-dep-mac.local>
2022-06-24 11:47:06 -0500

    updated

--- src/main/java/edu/mcw/scge/controller/ToolkitController.java
+++ src/main/java/edu/mcw/scge/controller/ToolkitController.java
@@ -7,6 +7,7 @@ import edu.mcw.scge.datamodel.*;
 import edu.mcw.scge.service.DataAccessService;
 import edu.mcw.scge.service.db.DBService;
 import edu.mcw.scge.service.es.IndexServices;
+import org.json.JSONObject;
 import org.elasticsearch.action.search.SearchResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +23,7 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -42,6 +44,79 @@ public class ToolkitController {
 
         return null;
     }
+
+    @RequestMapping(value="/feedback")
+    public String getFeedback(HttpServletRequest req, HttpServletResponse res, Model model) throws Exception {
+
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+            buffer.append(System.lineSeparator());
+        }
+        JSONObject obj = new JSONObject(buffer.toString());
+        reader.close();
+
+
+        String email = (String) obj.get("email");
+        String feedbackMsg = (String) obj.get("message");
+        String page = (String) obj.get("webPage");
+
+
+        FeedbackDao fdao = new FeedbackDao();
+        fdao.insert(email, feedbackMsg,page);
+
+
+            String smtpHost = "smtp.mcw.edu";
+
+            // Get a Properties object
+            Properties props = System.getProperties();
+
+            props.setProperty("mail.smtp.host", "smtp.mcw.edu");
+            props.setProperty("mail.smtp.port", "25");
+
+            Session session = Session.getInstance(props, null);
+
+            // -- Create a new message --
+            final MimeMessage msg = new MimeMessage(session);
+
+            // -- Set the FROM and TO fields --
+            msg.setFrom(new InternetAddress("scge_toolkit@mcw.edu", "SCGE Toolkit"));
+
+           // msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("scge_toolkit@mcw.edu", false));
+         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("scge_toolkit@mcw.edu", false));
+
+            //msg.setRecipients(Message.RecipientType.BCC, InternetAddress.parse("jdepons@mcw.edu", false));
+
+            msg.setSubject("SCGE Toolkit Feedback Request");
+
+
+            String emailBody = "";
+
+            emailBody += "\n\nFeedback message received from :\t" + email + "\n\n";
+            emailBody += feedbackMsg + "\n\n";
+            emailBody += "This message was sent from :\t" + page + "\n\n";
+
+
+            msg.setText(emailBody, "utf-8");
+            msg.setSentDate(new Date());
+
+            SMTPTransport t = (SMTPTransport)session.getTransport("smtp");
+
+            t.connect();
+            t.sendMessage(msg, msg.getAllRecipients());
+            t.close();
+
+
+
+
+
+
+        return "requestAccount";
+    }
+
+
 
     @RequestMapping(value="/requestAccount")
     public String getRequestAccount(HttpServletRequest req, HttpServletResponse res, Model model) throws Exception {
